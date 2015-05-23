fde6119ba8c77eb62b419e4cf4c021497e077597
Stéphane MORI <stephane.mori@gmail.com>
2015-05-24 01:11:08 +0200

    Commit overview event and init map exhibition

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $('.carousel').carousel();
 
-function initialize() {
+function initializeMapContact() {
     var mapOptions = {
         zoom: 16
     };
@@ -20,4 +20,39 @@ function initialize() {
             alert("Problem with geolocation");
     });
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+
+function initializeMapExhibition() {
+    var mapOptions = {
+        zoom: 16,
+        draggable: false,
+        userZoom: false,
+        disableDoubleClickZoom: false,
+        disableDefaultUI: true,
+        panControl: false,
+        zoomControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        overviewMapControl: false,
+        rotateControl: false,
+        scrollwheel: false
+    };
+
+    var map = new google.maps.Map(document.getElementById('map-exhibition'),
+        mapOptions);
+
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'address': "2 bis rue Charles Lamoureux 33000 Bordeaux" }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location,
+            });
+        } else
+            alert("Problem with geolocation");
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initializeMapContact);
+google.maps.event.addDomListener(window, 'load', initializeMapExhibition);
